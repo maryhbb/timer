7fe3962b612a54e2ebb82f4716e47e0685be1b9e
Mary <mary_hbb@yahoo.com>
2023-04-16 23:11:29 +0200

    Implement updateTimer func

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ const timerEl = document.getElementById("timer");
 
 let interval;
 let timeLeft = 1500;
+
+function updateTimer() {
+    let minutes = Math.floor(timeLeft / 60);
+    let seconds = timeLeft % 60;
+    let formattedTime = `${minutes.toString().padStart(2, "0")}:${seconds
+      .toString()
+      .padStart(2, "0")}`;
+  
+    timerEl.innerHTML = formattedTime;
+  }
