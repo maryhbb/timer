deb268f9aaae09aea18180e97cfaa20283706d52
Mary <mary_hbb@yahoo.com>
2023-04-16 23:14:22 +0200

    Format Js

--- index.js
+++ index.js
@@ -7,38 +7,38 @@ let interval;
 let timeLeft = 1500;
 
 function updateTimer() {
-    let minutes = Math.floor(timeLeft / 60);
-    let seconds = timeLeft % 60;
-    let formattedTime = `${minutes.toString().padStart(2, "0")}:${seconds
-      .toString()
-      .padStart(2, "0")}`;
-  
-    timerEl.innerHTML = formattedTime;
-  }
+  let minutes = Math.floor(timeLeft / 60);
+  let seconds = timeLeft % 60;
+  let formattedTime = `${minutes.toString().padStart(2, "0")}:${seconds
+    .toString()
+    .padStart(2, "0")}`;
 
-  function startTimer() {
-    interval = setInterval(() => {
-      timeLeft--;
-      updateTimer();
-      if (timeLeft === 0) {
-        clearInterval(interval);
-        alert("Time's up!");
-        timeLeft = 1500;
-        updateTimer();
-      }
-    }, 1000);
-  }
+  timerEl.innerHTML = formattedTime;
+}
 
-  function stopTimer() {
-    clearInterval(interval);
-  }
-
-  function resetTimer() {
-    clearInterval(interval);
-    timeLeft = 1500;
+function startTimer() {
+  interval = setInterval(() => {
+    timeLeft--;
     updateTimer();
-  }
+    if (timeLeft === 0) {
+      clearInterval(interval);
+      alert("Time's up!");
+      timeLeft = 1500;
+      updateTimer();
+    }
+  }, 1000);
+}
 
-  startEl.addEventListener("click", startTimer);
+function stopTimer() {
+  clearInterval(interval);
+}
+
+function resetTimer() {
+  clearInterval(interval);
+  timeLeft = 1500;
+  updateTimer();
+}
+
+startEl.addEventListener("click", startTimer);
 stopEl.addEventListener("click", stopTimer);
 resetEl.addEventListener("click", resetTimer);
