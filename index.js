d554013185da0ec60c904ece4efa74b02c856d92
Mary <mary_hbb@yahoo.com>
2023-04-16 22:22:47 +0200

    Implement Variables

---
+++ index.js
@@ -0,0 +1,4 @@
+const startEl = document.getElementById("start");
+const stopEl = document.getElementById("stop");
+const resetEl = document.getElementById("reset");
+const timerEl = document.getElementById("timer");
