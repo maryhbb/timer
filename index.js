60e8dd9267cbb9e4f1ec441400b866a956a9827f
Mary <mary_hbb@yahoo.com>
2023-04-16 22:23:34 +0200

    Set Interval

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ const startEl = document.getElementById("start");
 const stopEl = document.getElementById("stop");
 const resetEl = document.getElementById("reset");
 const timerEl = document.getElementById("timer");
+
+let interval;
+let timeLeft = 1500;
